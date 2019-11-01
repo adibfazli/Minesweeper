723db57b30675285b19adec35740c52a6081f51c
Adib Fazli <adibfazli@Adibs-MacBook-Pro.local>
2019-11-01 12:37:29 -0700

    menu button working

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
 //-----------------CONST and VAR-------------------
-var height , width , mineCount , column , row , board , bombPercenterg , bombArray , bombRandom , newDiv;
+var height , width , mineCount , column , row , board , bombPercentage , bombArray , bombRandom , newDiv , allDivs , logo , menu , size , difficulty , play , container , reset , refresh;
 
 //-------------------CACHED-----------------------
 bombArray = []
-var logo = document.querySelector(".logo")
-var menu = document.querySelector(".menu")
+logo = document.querySelector(".logo")
+menu = document.querySelector(".menu")
 
-var size = document.getElementById('select');
-var difficulty = document.getElementById('difficulty');
+size = document.getElementById('select');
+difficulty = document.getElementById('difficulty');
 
-var play = document.getElementById('play')
+play = document.getElementById('play')
 
-var container = document.querySelector('.container');
-var reset = document.querySelector('.reset')
-var refresh = document.querySelector('.refresh')
+container = document.querySelector('.container');
+reset = document.querySelector('.reset')
+refresh = document.querySelector('.refresh')
 
-
+allDivs = document.createElement('div').classList.add('Alldivs')
 
 //------------------EVENT LISTENER-----------------
 container.addEventListener('click' , clickHandler);
 
 play.addEventListener('click', playHandler)
 
+reset.addEventListener('click' , init)
+refresh.addEventListener('click' , playHandler)
+
 //--------------------FUNCTION---------------------
 //size
 function playHandler(){
     menu.style.display = 'none'
     logo.style.display = 'none'
+    bombArray = []
+    if (document.querySelector('.allDivs')) document.querySelector('.allDivs').remove()
+    allDivs = document.createElement('div')
+    allDivs.classList.add('allDivs')
+    container.appendChild(allDivs)
+    
     if(size.value === 'Small'){
         column = 10
         row = 10
@@ -39,17 +48,17 @@ function playHandler(){
     }
     //difficulty
     if(difficulty.value === 'Easy'){
-        bombPercenterg = (column*row)*0.1
+        bombPercentage = (column*row)*0.1
     }else if (difficulty.value === 'Medium' ){
-        bombPercenterg = (column*row)*0.2
+        bombPercentage = (column*row)*0.2
     }else {
-        bombPercenterg = (column*row)*0.3
+        bombPercentage = (column*row)*0.3
     }
-    container.style["grid-template-columns"] = `repeat(${column}, 31px)`;
-    container.style["grid-template-row"] = `repeat(${row}, 31px)`;
+    allDivs.style["grid-template-columns"] = `repeat(${column}, 31px)`;
+    allDivs.style["grid-template-row"] = `repeat(${row}, 31px)`;
     //setting the board
     board = new Array((column*row) -1).fill(0);
-    for (i=0 ; i < bombPercenterg ; i++){
+    for (i=0 ; i < bombPercentage ; i++){
         bombRandom = Math.floor(Math.random()*(column*row))
         bombArray.push(bombRandom);
 
@@ -66,7 +75,7 @@ function render(){
     for(i=0 ; i <(column * row) ; i++){
         newDiv = document.createElement('div');
         newDiv.setAttribute('id' , i)
-        container.appendChild(newDiv)
+        allDivs.appendChild(newDiv)
         if (bombArray.includes(i)){
             newDiv.setAttribute('class' , 'bomb')
         }
@@ -76,12 +85,7 @@ function render(){
 }
 
 function init(){
+    allDivs.style.display = 'none'
+    menu.style.display = 'block'
+    logo.style.display = 'block'
 }
-
-
-
-
-
-
-
-
